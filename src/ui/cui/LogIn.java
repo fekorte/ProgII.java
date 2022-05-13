a96d023e6c73a0c2f82e29065729d4f44cf5b8e9
fekorte <fe.korte@web.de>
2022-05-13 13:47:32 +0200

    Login beginning

--- src/ui/cui/LogIn.java
+++ src/ui/cui/LogIn.java
@@ -1,7 +1,44 @@
 package ui.cui;
 
-public class LogIn {
-    public static void main(String[] args){
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.List;
 
+public class LogIn {
+    private BufferedReader in;
+
+    public static void main(String[] args) {
+        private void showMenue() {
+            System.out.print("Commands: \n  Login:  'l'");
+            System.out.print("         \n  Register:  'r'");
+            System.out.print("         \n  ---------------------");
+            System.out.println("         \n  Beenden:        'q'");
+            System.out.print("> "); // Prompt
+            System.out.flush(); // ohne NL ausgeben
+        }
+    }
+
+    private String readInput() throws IOException {
+        // einlesen von Konsole
+        return in.readLine();
+    }
+
+    private void processInput(String line) throws IOException {
+        String userName;
+        String password;
+        List<> liste;
+
+
+        switch (line) {
+            case "l":
+                System.out.print("Username > ");
+                userName = readInput();
+                System.out.print("Password > ");
+                password = readInput();
+                break;
+            case "r":
+
+                break;
+        }
     }
 }

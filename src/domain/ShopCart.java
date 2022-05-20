b0d0b5d1ced6e348785032002971aeb4366a0127
sedefnurc <105489965+sedefnurc@users.noreply.github.com>
2022-05-20 14:34:32 +0200

    Changes

--- src/domain/ShopCart.java
+++ src/domain/ShopCart.java
@@ -1,18 +1,51 @@
 package domain;
 
 import valueobjects.Client;
+import valueobjects.Employee;
 import valueobjects.Item;
+
+import java.lang.reflect.MalformedParameterizedTypeException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ShopCart {
     private Client client;
     private int date;
     private Item item;
 
+List<Item> cartItems= new ArrayList<Item>();
+
+
+    public void putItemsInCart(Item item){
+        cartItems.add(item);
+    }
+
+    public void changeNumberItemsInCart(Item item, int num){
+        cartItems.set(num,item);
+    }
+
+
+    public void emptyCart(){
+        cartItems.removeAll(cartItems);
+    }
+
+    public void removeItemsFromCart(Item item){
+        cartItems.remove(item);
+    }
+
+
+
     public ShopCart(Client client, int date, Item item){
         this.client = client;
         this.date = date;
-        this.item = item;
+
     }
+
+
+
+
     // putItemsInCart();
     //
     // changeNumberItemsInCart();
--- src/valueobjects/Item.java
+++ src/valueobjects/Item.java
@@ -49,7 +49,14 @@ public class Item {
 
     public int getItemCode(){ return itemCode;}
 
+<<<<<<< Updated upstream
     public int getNumberInStock() {return numberInStock;}
+=======
+    public int getStock() {return stock;}
+
+
+
+>>>>>>> Stashed changes
 }
 
 

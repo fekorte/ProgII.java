8096a853aa1e7b2ab2329d1b2d56a5ca258ce141
NathaMora <nathyk.buzo@gmail.com>
2022-05-27 12:44:03 +0200

    da
    ignore

---
+++ src/domain/Receipt.java
@@ -0,0 +1,41 @@
+package domain;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class Receipt {
+
+  //  public Receipt {
+
+//   }
+
+    //private ShopCart purchasedItems= cartItems;  --> totally unsure about it
+
+    //this class will take into String all what is im  the ShopCart and the Client information
+    //then it will print it in a file.txt
+    //then the ShopCart will be close
+
+    //This par of Frankie comes from File Persistence manager--> if we are not working with database the interface doenst make sense
+ //   private BufferedReader reader = null;
+   // private PrintWriter writer = null;
+
+   // public void openForReading(String datei) throws FileNotFoundException {
+     //   reader = new BufferedReader(new FileReader(datei));
+    //}
+
+    //public void openForWriting(String datei) throws IOException {
+      //  writer = new PrintWriter(new BufferedWriter(new FileWriter(datei)));
+    }
+
+
+
+
+
+
